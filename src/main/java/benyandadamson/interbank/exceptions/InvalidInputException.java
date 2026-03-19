3a7ef52249891df9f384a15d331fa8df5c31f597
OlaAdams <adamsonoladipupo>
2026-03-19 21:06:45 +0100

    InvalidInputException Added Co-authored-by: Beny <benynouba@gmail.com>

---
+++ src/main/java/benyandadamson/interbank/exceptions/InvalidInputException.java
@@ -0,0 +1,7 @@
+package benyandadamson.interbank.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
