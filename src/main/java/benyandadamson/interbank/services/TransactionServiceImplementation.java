acecfdcaad37909936d74c85e95c67da61790ffb
OlaAdams <adamsonoladipupo>
2026-03-27 06:14:41 +0100

    added saving deposit as a transaction

--- src/main/java/benyandadamson/interbank/services/TransactionServiceImplementation.java
+++ src/main/java/benyandadamson/interbank/services/TransactionServiceImplementation.java
@@ -1,7 +1,11 @@
 package benyandadamson.interbank.services;
 
 import benyandadamson.interbank.data.models.Account;
+import benyandadamson.interbank.data.models.Transaction;
+import benyandadamson.interbank.data.models.TransactionStatus;
+import benyandadamson.interbank.data.models.TransactionType;
 import benyandadamson.interbank.data.repositories.AccountRepository;
+import benyandadamson.interbank.data.repositories.TransactionRepository;
 import benyandadamson.interbank.dtos.requests.DepositRequest;
 import benyandadamson.interbank.dtos.requests.TransferRequest;
 import benyandadamson.interbank.dtos.requests.WithdrawRequest;
@@ -11,10 +15,15 @@ import benyandadamson.interbank.dtos.responses.WithdrawResponse;
 import benyandadamson.interbank.exceptions.AccountNumberNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class TransactionServiceImplementation implements TransactionService{
 
     @Autowired
     public AccountRepository accountRepository;
+    @Autowired
+    public TransactionRepository transactionRepository;
 
     @Override
     public DepositResponse deposit(DepositRequest depositRequest) {
@@ -24,9 +33,19 @@ public class TransactionServiceImplementation implements TransactionService{
 //            also validate supplied password
             account.setBalance(depositRequest.getAmount());
 
+            Transaction transaction = new Transaction();
+            transaction.setTransactionStatus(TransactionStatus.SUCCESS);
+            transaction.setTransactionType(TransactionType.DEPOSIT);
+            transaction.setCreatedAt(LocalDateTime.now());
+            transaction.setDestinationAccount(account);
+            transactionRepository.save(transaction);
+
+
+
             String message = "Amount deposited successfully";
             DepositResponse depositResponse = new DepositResponse(account.getAccountNumber(), message);
             return depositResponse;
+
 
         }
         else throw new AccountNumberNotFoundException ("Account number not found");
