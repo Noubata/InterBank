3bfe3b1a3a7b805691bd4b33b0c2b705285b3f98
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-06 03:35:06 +0100

    updated transaction sercive implimentation

--- src/main/java/benyandadamson/interbank/services/TransactionServiceImplementation.java
+++ src/main/java/benyandadamson/interbank/services/TransactionServiceImplementation.java
@@ -13,6 +13,7 @@ import benyandadamson.interbank.dtos.responses.DepositResponse;
 import benyandadamson.interbank.dtos.responses.TransferResponse;
 import benyandadamson.interbank.dtos.responses.WithdrawResponse;
 import benyandadamson.interbank.exceptions.AccountNumberNotFoundException;
+import benyandadamson.interbank.exceptions.WrongPasswordException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
@@ -27,37 +28,56 @@ public class TransactionServiceImplementation implements TransactionService{
 
     @Override
     public DepositResponse deposit(DepositRequest depositRequest) {
-        if (accountRepository.findByAccountNumber(depositRequest.getAccountNumber()).isPresent()){
-            Account account = accountRepository.findByAccountNumber(depositRequest.getAccountNumber()).get();
-//           implement passwordEncoder to check if password is correct supplied by the user
-//            also validate supplied password
-            account.setBalance(depositRequest.getAmount());
+        validateAccountNumber(depositRequest.getAccountNumber());
+        Account account = accountRepository.findByAccountNumber(depositRequest.getAccountNumber()).get();
+        account.setBalance(depositRequest.getAmount());
 
-            Transaction transaction = new Transaction();
-            transaction.setTransactionStatus(TransactionStatus.SUCCESS);
-            transaction.setTransactionType(TransactionType.DEPOSIT);
-            transaction.setCreatedAt(LocalDateTime.now());
-            transaction.setDestinationAccount(account);
-            transactionRepository.save(transaction);
+        Transaction transaction = new Transaction();
+        transaction.setTransactionStatus(TransactionStatus.SUCCESS);
+        transaction.setTransactionType(TransactionType.DEPOSIT);
+        transaction.setCreatedAt(LocalDateTime.now());
+        transaction.setDestinationAccount(account);
+        transactionRepository.save(transaction);
 
+        String message = "Amount deposited successfully";
+        DepositResponse depositResponse = new DepositResponse(account.getAccountNumber(), message);
+        return depositResponse;
 
-
-            String message = "Amount deposited successfully";
-            DepositResponse depositResponse = new DepositResponse(account.getAccountNumber(), message);
-            return depositResponse;
-
-
-        }
-        else throw new AccountNumberNotFoundException ("Account number not found");
     }
 
     @Override
     public WithdrawResponse withdraw(WithdrawRequest withdrawRequest) {
-        return null;
+        validateAccountNumber(withdrawRequest.getAccount().getAccountNumber());
+        validatePassword(withdrawRequest.getPassword(), withdrawRequest.getAccount().getAccountNumber());
+        Account account = accountRepository.findByAccountNumber(withdrawRequest.getAccount().getAccountNumber()).get();
+        account.setBalance(account.getBalance().subtract(withdrawRequest.getAmount()));
+
+        Transaction transaction = new Transaction();
+        transaction.setTransactionStatus(TransactionStatus.SUCCESS);
+        transaction.setTransactionType(TransactionType.WITHDRAW);
+        transaction.setCreatedAt(LocalDateTime.now());
+        transaction.setDestinationAccount(account);
+        transactionRepository.save(transaction);
+
+        String message = "Amount withdrawn successfully";
+        return new WithdrawResponse(message);
     }
 
     @Override
     public TransferResponse transfer(TransferRequest transferRequest) {
         return null;
     }
+
+    private void validateAccountNumber(String accountNumber){
+        if (!accountRepository.findByAccountNumber(accountNumber).isPresent()){
+            throw new AccountNumberNotFoundException ("Account number not found");
+        }
+    }
+    private void validatePassword(String password, String accountNumber){
+        Account account = accountRepository.findByAccountNumber(accountNumber).get();
+        if(!account.getPassword().matches(password)){
+            throw new WrongPasswordException("Sorry, your password is incorrect");
+        }
+    }
+
 }
